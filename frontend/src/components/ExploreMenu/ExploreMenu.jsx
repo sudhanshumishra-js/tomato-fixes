2f737b1dc62630c1dc4b10420102868cfb20e892
Sudhanshu Mishra <smishra007123@gmail.com>
2025-07-24 18:22:14 +0530

    resolved stash conflicts

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ExploreMenu.css'
 import { menu_list } from '../../assets/assets'
 
 const ExploreMenu = ({category, setCategory}) => {
+
+  const [hoverId, setHoverId] = useState(null);
+
+  const handleMouseEnter = (id) => {
+    setHoverId(id);
+  };
+
+  const handleMouseLeave = () => {
+    setHoverId(null);
+  };
 
   return (
     <div className='explore-menu' id='explore-menu'>
@@ -11,8 +21,11 @@ const ExploreMenu = ({category, setCategory}) => {
         <div className="explore-menu-list">
             {menu_list.map((item,index)=>{
                 return (
-                    <div onClick={()=>setCategory(prev=> prev === item.menu_name ? 'All' : item.menu_name)} key={index} className="explore-menu-list-item">
-                        <img className={category===item.menu_name?'active':''} src={item.menu_image} alt="" />
+                    <div onClick={()=>setCategory(prev=> prev === item.menu_name ? 'All' : item.menu_name)} key={index} className= "explore-menu-list-item" 
+                    onMouseEnter={() => handleMouseEnter(item.id)} 
+                    onMouseLeave={handleMouseLeave}
+                    >
+                        <img className={category===item.menu_name?'active':'' + (hoverId === item.id ? 'hovered' : '')} src={item.menu_image} alt="" />
                         <p>{item.menu_name}</p>
                     </div>
                 )
