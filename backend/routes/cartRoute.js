2322c6165da62dcef706702b2bb0717f8c491e50
Sudhanshu Mishra <42689634+sudhanshumishra-js@users.noreply.github.com>
2025-09-29 22:12:17 +0530

    lets go

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -6,6 +6,7 @@ const cartRouter = express.Router();
 
 cartRouter.post("/add", authMiddleware, addToCart)
 cartRouter.post("/remove",authMiddleware, removeFromCart)
-cartRouter.post("/get",authMiddleware, getCart)
+cartRouter.post("/get",authMiddleware, getCart) 
+cartRouter.post("/get", authMiddleware,getCart)
 
 export default cartRouter;
